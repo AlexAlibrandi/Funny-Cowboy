f6fccd02ca135bfbb64529f755b00700eac57f62
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-02-11 10:16:40 -0500

    Created Spring boot project

---
+++ src/main/java/com/spring5/funnycowboy/FunnyCowboyApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.funnycowboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyCowboyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunnyCowboyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring5/funnycowboy/FunnyCowboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.funnycowboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyCowboyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
