0d2caf5d4375dcd6934abaa63d96cc1dabd07e5e
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-02-11 11:00:43 -0500

    Created controller to help display random joke on front end

---
+++ src/main/java/com/spring5/funnycowboy/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.spring5.funnycowboy.controllers;
+
+import com.spring5.funnycowboy.services.JokeService;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
